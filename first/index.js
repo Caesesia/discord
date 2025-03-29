4af38414d60875e7840cc6b8fbf4748e8009b9bf
caesesia <marsel.msc@proton.me>
2025-03-29 16:05:54 +0100

    Fichier index - propre

---
+++ first/index.js
@@ -0,0 +1,16 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const CLIENT = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once).
+// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
+// It makes some properties non-nullable.
+CLIENT.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+CLIENT.login(token);
