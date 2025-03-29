4fe15185ea459aef9833ad1b4b0c594c26c869d4
caesesia <marsel.msc@proton.me>
2025-03-29 18:41:07 +0100

    Fichier index - progression

--- first/index.js
+++ first/index.js
@@ -1,6 +1,9 @@
+const FS = require('node:fs');
+const PATH = require('node:path');
+
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { TOKEN } = require('./config.json');
 
 // Create a new client instance
 const CLIENT = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -13,4 +16,29 @@ CLIENT.once(Events.ClientReady, readyClient => {
 });
 
 // Log in to Discord with your client's token
-CLIENT.login(token);
+CLIENT.login(TOKEN);
+
+CLIENT.commands = new Collection();
+
+const FOLDERS_PATH = PATH.join(__dirname, 'commands');
+const COMMAND_FOLDERS = FS.readdirSync(FOLDERS_PATH);
+
+for (const FOLDER of COMMAND_FOLDERS) {
+	const COMMANDS_PATH = PATH.join(FOLDERS_PATH, FOLDER);
+	const COMMAND_FILES = FS.readdirSync(COMMANDS_PATH).filter(file => file.endsWith('.js'));
+	for (const FILE of COMMAND_FILES) {
+		const FILE_PATH = PATH.join(COMMANDS_PATH, FILE);
+		const COMMAND = require(FILE_PATH);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in COMMAND && 'execute' in COMMAND) {
+			CLIENT.commands.set(COMMAND.data.name, COMMAND);
+		} else {
+			console.log(`[WARNING] The command at ${FILE_PATH} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+CLIENT.on(Events.InteractionCreate, interaction => {
+    if (!interaction.isChatInputCommand()) return;
+	console.log(interaction);
+});
