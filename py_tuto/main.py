7543a987299150602a566497ea30185fb506d108
caesesia <marsel.msc@proton.me>
2025-04-12 02:01:10 +0200

    à clean et compléter - scripts séparés

--- py_tuto/main.py
+++ py_tuto/main.py
@@ -2,13 +2,22 @@
 
 
 from dotenv import load_dotenv
+
 import os
 import random
 import discord
 
+from snitch import snitch
+
 
 load_dotenv()
-token = os.getenv("TOKEN")
+TOKEN = os.getenv("TOKEN")
+HOMOS = int(os.getenv("HOMOS"))
+LOGOS = int(os.getenv("LOGOS"))
+PATHOS = int(os.getenv("PATHOS"))
+TACOS = int(os.getenv("TACOS"))
+POPOS = int(os.getenv("POPOS"))
+MYNTHOS = int(os.getenv("MYNTHOS"))
 
 
 intents = discord.Intents.default()
@@ -36,17 +45,18 @@ async def on_message(message):
     elif msg == "quoi" or msg == "quoi?":
         await message.reply("FEUR")
 
-    elif message.author.id == 757771138678915142:
+    elif "https://" in message.content and "elsword" in message.content or "https://" in message.content and "nerd" in message.content:
+        await message.reply("Nerd :nerd:")
+
+    elif message.author.id == LOGOS:
         await message.reply(choix)
-
-
 
 @client.event
 async def on_message_delete(message):
-    if message.author == client.user:
+    if message.author == message.guild.me:
         return
 
-    await message.channel.send(f'/!\\ {message.author} n\'assume pas d\'avoir dit "{message.content}"')
+    await snitch(message)
 
 
-client.run(token)
+client.run(TOKEN)
---
+++ py_tuto/snitch.py
@@ -0,0 +1,2 @@
+async def snitch(message):
+    await message.channel.send(f'/!\\ {message.author} n\'assume pas d\'avoir dit "{message.content}"')
